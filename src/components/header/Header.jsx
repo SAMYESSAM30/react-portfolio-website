cb5c77e45ba82f9fdf39e8b5d254bcc06ec8afaf
SAMYESSAM30 <samyessam2000@gmail.com>
2023-10-16 16:46:47 +0300

    s

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">Smith</a>
+        <a href="index.html" className="nav__logo">Samy</a>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">

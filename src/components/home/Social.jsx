ad62f1d6245f7e1b8f9e1f4bd50207da8ba08302
SAMYESSAM30 <samyessam2000@gmail.com>
2023-10-16 11:13:11 +0300

    update

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/samy-essam-bb2411236/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://www.facebook.com/profile.php?id=100008369070405&mibextid=LQQJ4d"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-facebook-f"></i>
+      </a>
+      <a
+        href="https://github.com/SAMYESSAM30"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;

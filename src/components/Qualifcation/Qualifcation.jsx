a3113af858d41033ee2211752fb614f671dfab56
SAMYESSAM30 <samyessam2000@gmail.com>
2023-11-01 12:38:30 +0200

    c

--- src/components/Qualifcation/Qualifcation.jsx
+++ src/components/Qualifcation/Qualifcation.jsx
@@ -1,33 +1,191 @@
-import React from 'react'
-import './qualifcation.css'
+import React, { useState } from "react";
+import "./qualifcation.css";
 const Qualifcation = () => {
+  const [toggleState, setToggleState] = useState(1);
+  const toggleTop = (index) => {
+    setToggleState(index);
+  };
   return (
     <section className="qualifcation section" id="qualifcation">
       <h2 className="section__title">Qualifcation</h2>
-      <span className="section__subtitle">
-        My Personel journey
-      </span>
+      <span className="section__subtitle">My Personel journey</span>
 
-      <div className="qualifcation__container container grid">
+      <div className="qualifcation__container container ">
         <div className="qualifcation__tabs">
-          <div className="qualifcation__button qualifcation__active button--flex">
-            <i className='uil uil-graduation-cap qualifcation__icon'>Education</i>
-
-            <div className="qualifcation__button qualifcation__active button--flex">
-              <i className='uil uil-briefcase-alt qualifcation__icon'>Education</i>
+          <div
+            className={
+              toggleState === 1
+                ? "qualifcation__button qualifcation__active button--flex"
+                : "qualifcation__button button--flex"
+            }
+            onClick={() => toggleTop(1)}
+          >
+            <i className="uil uil-graduation-cap qualifcation__icon"></i>
+            Education
+          </div>
+          <div
+            className={
+              toggleState === 2
+                ? "qualifcation__button qualifcation__active button--flex"
+                : "qualifcation__button button--flex"
+            }
+            onClick={() => toggleTop(2)}
+          >
+            <i className="uil uil-briefcase-alt qualifcation__icon">
+              Experience
+            </i>
+          </div>
+        </div>
+        <div className="qualifcation__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualifcation__content qualifcation__content-active"
+                : "qualifcation__content "
+            }
+          >
+            {/* Data1 */}
+            <div className="qualifcation__data">
+              {" "}
+              <div>
+                <h3 className="qualifcation__title">Web Design</h3>
+                <span className="qualifcation__subtitle">
+                  spain - Institute
+                </span>
+                <div className="qualifcation__calender">
+                  <i className="uil uil-calendar-alt">2021 - present</i>
+                </div>
+              </div>
+              <div>
+                <span className="qualifcation__rounder"></span>
+                <span className="qualifcation__line"></span>
+              </div>
             </div>
+            {/* Data2 */}
+            <div className="qualifcation__data">
+              <div></div>
+              <div>
+                <span className="qualifcation__rounder"></span>
+                <span className="qualifcation__line"></span>
+              </div>{" "}
+              <div>
+                <h3 className="qualifcation__title">Art Director</h3>
+                <span className="qualifcation__subtitle">
+                  spain - Institute
+                </span>
+                <div className="qualifcation__calender">
+                  <i className="uil uil-calendar-alt"> 2021 - 2022 </i>
+                </div>
+              </div>
+            </div>
+            {/* Data3 */}
+            <div className="qualifcation__data">
+              {" "}
+              <div>
+                <h3 className="qualifcation__title">Web development</h3>
+                <span className="qualifcation__subtitle">
+                  spain - Institute
+                </span>
+                <div className="qualifcation__calender">
+                  <i className="uil uil-calendar-alt">2021 - present</i>
+                </div>
+              </div>
+              <div>
+                <span className="qualifcation__rounder"></span>
+                <span className="qualifcation__line"></span>
+              </div>
+            </div>
+            {/* Data4 */}
+            <div className="qualifcation__data">
+              <div></div>
+              <div>
+                <span className="qualifcation__rounder"></span>
+                <span className="qualifcation__line"></span>
+              </div>{" "}
+              <div>
+                <h3 className="qualifcation__title">UX Expert</h3>
+                <span className="qualifcation__subtitle">
+                  spain - Institute
+                </span>
+                <div className="qualifcation__calender">
+                  <i className="uil uil-calendar-alt"> 2019 - 2018 </i>
+                </div>
+              </div>
+            </div>
+            <div></div>
+          </div>
+
+          {/*  */}
+
+          <div
+            className={
+              toggleState === 2
+                ? "qualifcation__content qualifcation__content-active"
+                : "qualifcation__content "
+            }
+          >
+            {/* Data1 */}
+            <div className="qualifcation__data">
+              {" "}
+              <div>
+                <h3 className="qualifcation__title">Product Designer</h3>
+                <span className="qualifcation__subtitle">
+                  spain - Institute
+                </span>
+                <div className="qualifcation__calender">
+                  <i className="uil uil-calendar-alt">2021 - present</i>
+                </div>
+              </div>
+              <div>
+                <span className="qualifcation__rounder"></span>
+                <span className="qualifcation__line"></span>
+              </div>
+            </div>
+            {/* Data2 */}
+            <div className="qualifcation__data">
+              <div></div>
+              <div>
+                <span className="qualifcation__rounder"></span>
+                <span className="qualifcation__line"></span>
+              </div>{" "}
+              <div>
+                <h3 className="qualifcation__title">UX Designer</h3>
+                <span className="qualifcation__subtitle">
+                  spain - Institute
+                </span>
+                <div className="qualifcation__calender">
+                  <i className="uil uil-calendar-alt"> 2021 - 2022 </i>
+                </div>
+              </div>
+            </div>
+            {/* Data3 */}
+            <div className="qualifcation__data">
+              {" "}
+              <div>
+                <h3 className="qualifcation__title">Web Designer</h3>
+                <span className="qualifcation__subtitle">Figma - spain</span>
+                <div className="qualifcation__calender">
+                  <i className="uil uil-calendar-alt">2021 - present</i>
+                </div>
+              </div>
+              <div>
+                <span className="qualifcation__rounder"></span>
+                <span className="qualifcation__line"></span>
+              </div>
+            </div>
+
+            <div></div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Qualifcation;
 // const data = [
 //   {
 //     icon: <i className='uil uil-graduation-cap qualifcation__icon'>Education</i>,
-
 
 //   },
 //   {},

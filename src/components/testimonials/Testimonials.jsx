3a08083408b71350fa73c85f125aed86b6fae599
samy <samyessam2000@gami.com>
2023-11-19 17:24:20 +0200

    update

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,21 +3,47 @@ import Img1 from '../../assets/testimonial1.png'
 import Img2 from '../../assets/testimonial2.png'
 import Img3 from '../../assets/testimonial3.png'
 import './testimonials.css'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import required modules
+import { Pagination } from 'swiper/modules';
 const Testimonials = () => {
     return (
         <section className='testimonial container section'><h2 h2 className="section__title" > My Clients say</h2>
             <span className="section__subtitle">Testimonial</span>
-            <div className='testimonial__container'>
+            <Swiper
+                looo={true}
+                grabCursor={true}
+                spaceBetween={24}
+                pagination={{
+                    clickable: true,
+                }}
+                breakpoints={{
+                    576: {
+                        slidesPerView: 2,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 48,
+                    },
+
+                }}
+                modules={[Pagination]}
+                className='testimonial__container'>
                 {data.map((item, index) => {
                     return (
-                        <div className='testimonial__card' kay={index}>
+                        <SwiperSlide className='testimonial__card' kay={index}>
                             <img src={item.image} alt='' className='testimonial__img'></img>
                             <h3 className='testimonial__name'>{item.title}</h3>
                             <p className='testimonial__description'>{item.descriptio}</p>
-                        </div>
+                        </SwiperSlide>
                     )
                 })}
-            </div>
+            </Swiper>
         </section>
     )
 }

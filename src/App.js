5925591c9e4c6beec503807055d2bddbb4614463
SAMYESSAM30 <samyessam2000@gmail.com>
2023-12-04 15:08:40 +0200

    ipdate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-
-import './App.css';
-import Services from './components/Services/Services';
-import Skills from './components/Skills/Skills';
-import About from './components/about/About';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import Qualifcation from './components/Qualifcation/Qualifcation';
-import Testimonials from './components/testimonials/Testimonials';
-import { Contact } from './components/contact/Contact';
-
+import "./App.css";
+import Services from "./components/Services/Services";
+import Skills from "./components/Skills/Skills";
+import About from "./components/about/About";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Qualifcation from "./components/Qualifcation/Qualifcation";
+import Testimonials from "./components/testimonials/Testimonials";
+import { Contact } from "./components/contact/Contact";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
       <Header />
-      <main className='main'>
+      <main className="main">
         <Home />
         <About />
         <Skills />
@@ -22,8 +21,8 @@ function App() {
         <Qualifcation />
         <Testimonials />
         <Contact />
+        <Footer />
       </main>
-
     </>
   );
 }
